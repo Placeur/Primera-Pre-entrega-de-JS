cf40fe7a112b431e819e1cf652ba70ba281b566c
Placeur <lucaspirizba@gmail.com>
2024-07-29 22:28:36 -0300

    v.0.0.1
    Primera pre-entrega de JS.

---
+++ Aumento de precios/algoritmo.js
@@ -0,0 +1,25 @@
+//Ejercicios con recursos aprendidos en clases//
+    
+//Aumento de un 30% a a precio de un producto//
+
+const multiplicacion = (a,b) => a * b;
+let precioNormal = 2500;
+let aumentoPorcentaje = 1.30;
+
+console.log("El precio final de su producto queda en:",precioNormal*aumentoPorcentaje)
+
+//Aumento de un 50% a precio de un producto//
+
+const multiplicacion1 = (a,b) => a * b;
+let precioNormal1 = 2500;
+let aumentoPorcentaje1 = 1.50;
+
+console.log("El precio final de su producto queda en:",precioNormal1*aumentoPorcentaje1)
+
+//Aumento de un 75% a precio de un producto//
+
+const multiplicacion2 = (a,b) => a * b;
+let precioNormal2 = 2500;
+let aumentoPorcentaje2 = 1.75;
+
+console.log("El precio final de su producto queda en:",precioNormal2*aumentoPorcentaje2)
